167670e89bd73be2c1ac31802e17a42cad181287
Ashbel Legros <ashbel.legros@gmail.com>
2026-07-14 18:43:07 -0400

    Update MainSystem.java to use GUI instead of CLI

--- src/MainSystem.java
+++ src/MainSystem.java
@@ -5,15 +5,13 @@
  * Class : MainSystem
  * Program Objective : Provide operator with a DMS to manage RaceCars loaded by a text file. They should be able to
  * print all race cars to screen, add a new race car with details, remove a race car, update a race car entry, and launch/simulate a race.
- * User should be able to exit the DMS as well using the corresponding command action.
+ * User should be able to exit the DMS as well using the corresponding exit button.
  * */
 
 public class MainSystem {
     public static void main(String[] args) {
         RaceCarManager raceCarService = new RaceCarManager();
 
-        CommandLineMenu system = new CommandLineMenu();
-
-        system.displayMenu(raceCarService);
+        MainFrame mainFrame = new MainFrame(raceCarService);
     }
 }

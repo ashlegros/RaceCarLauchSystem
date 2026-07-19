0805da00a5f2f06f1b855a53a59e968946c3f547
Ashbel Legros <ashbel.legros@gmail.com>
2026-07-19 18:10:03 -0400

    Update MainSystem.java to use DBManager

--- src/MainSystem.java
+++ src/MainSystem.java
@@ -3,14 +3,14 @@
  * Course : Software Development I
  * Date : 07/03/26
  * Class : MainSystem
- * Program Objective : Provide operator with a DMS to manage RaceCars loaded by a text file. They should be able to
+ * Program Objective : Provide operator with a DMS to manage RaceCars connected to a database. They should be able to
  * print all race cars to screen, add a new race car with details, remove a race car, update a race car entry, and launch/simulate a race.
  * User should be able to exit the DMS as well using the corresponding exit button.
  * */
 
 public class MainSystem {
     public static void main(String[] args) {
-        RaceCarManager raceCarService = new RaceCarManager();
+        RaceCarDBManager raceCarService = new RaceCarDBManager();
 
         MainFrame mainFrame = new MainFrame(raceCarService);
     }

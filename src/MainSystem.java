ad38fbb2da7b78dfb58519c61a52188cd1fcf372
Ashbel Legros <ashbel.legros@gmail.com>
2026-07-21 22:08:55 -0400

    Update MainSystem.java with documentation

--- src/MainSystem.java
+++ src/MainSystem.java
@@ -1,14 +1,14 @@
-/*
- * Name : Ashbel Legros
- * Course : Software Development I
- * Date : 07/03/26
- * Class : MainSystem
- * Program Objective : Provide operator with a DMS to manage RaceCars connected to a database. They should be able to
- * print all race cars to screen, add a new race car with details, remove a race car, update a race car entry, and launch/simulate a race.
- * User should be able to exit the DMS as well using the corresponding exit button.
- * */
-
+/**
+ * The class contains main and starts the program. The purpose is to provide operator with a DMS to manage RaceCars connected to
+ * a database. They should be able to print all race cars to screen, add a new race car with details,
+ * remove a race car, update a race car entry, and launch/simulate a race. User should be able to exit the DMS as well using the corresponding exit button.
+ *
+ * @author Ashbel Legros
+ **/
 public class MainSystem {
+    /**
+     * The main method which starts up the GUI.
+     */
     public static void main(String[] args) {
         RaceCarDBManager raceCarService = new RaceCarDBManager();
 

1517d5734625f819132d44587c740afbabe6fa00
Ashbel Legros <ashbel.legros@gmail.com>
2026-07-21 22:09:59 -0400

    Update RaceCarDBManager.java with documentation

--- src/RaceCarDBManager.java
+++ src/RaceCarDBManager.java
@@ -1,44 +1,64 @@
-/*
- * Name : Ashbel Legros
- * Course : Software Development I
- * Date : 07/16/26
- * Class : RaceCarDBManager
- * Purpose : Contains business logic to perform CRUD operations to the RaceCar database. It also performs a special function
- * to simulate a race for cars
- * */
-
 import DBHelper.RaceCars;
-
 import javax.swing.*;
 import java.sql.*;
 import java.util.ArrayList;
 
+/**
+ * This child class contains business logic to perform CRUD operations to the RaceCar database. It also
+ * performs a special function to simulate a race for cars. This drives the backend of the system as it performs
+ * all necessary action to complete CRUD functionality.
+ *
+ * @author Ashbel Legros
+ **/
 public class RaceCarDBManager extends RaceCarManager{
     private Connection connection;
     private RaceCars raceCarTable;
 
+    /**
+     * Initializes the manager with no connection and a query helper. The query helper (raceCarTable) handles
+     * database operations in SQL.
+     */
     public RaceCarDBManager() {
         connection = null;
         raceCarTable = new RaceCars();
     }
 
+    /**
+     * This method gets the connection value.
+     * @return Connection value of connection
+     */
     public Connection getConnection() {
         return connection;
     }
+
+    /**
+     * This method sets the value of connection the parameter.
+     * @param connection established connection of a database
+     */
     public void setConnection(Connection connection) {
         this.connection = connection;
     }
+
+    /**
+     * This method gets the value of raceCarTable.
+     * @return RaceCars the service that allows database operations
+     */
     public RaceCars getRaceCarTable() {
         return raceCarTable;
     }
+
+    /**
+     * This method sets the value of RaceCarTable to the parameter.
+     * @param raceCarTable the service that allows database operations
+     */
     public void setRaceCarTable(RaceCars raceCarTable) {
         this.raceCarTable = raceCarTable;
     }
 
-    /* Method: loadRaceCarDB()
-       Purpose: connects to sqlite database of choosing
-       Return Type: boolean
-       Arguments: String filePath
+    /**
+     * This establishes connection to the database.
+     * @param filePath the file path of the sample database
+     * @return boolean true or false value to determine if operation was successful
      */
     public boolean loadRaceCarDB(String filePath){
         try {
@@ -57,10 +77,10 @@ public class RaceCarDBManager extends RaceCarManager{
         return  true;
     }
 
-    /* Method: addRaceCar()
-       Purpose: adds a race car entry to the database
-       Return Type: boolean
-       Argument: RaceCar raceCar
+    /**
+     * This method adds a race car object to the database.
+     * @param raceCar object representing the race car
+     * @return boolean true or false value to determine if operation was successful
      */
     public boolean addRaceCar(RaceCar raceCar) {
         System.out.println("Adding Race Car...");
@@ -90,10 +110,10 @@ public class RaceCarDBManager extends RaceCarManager{
         return true;
     }
 
-    /* Method: removeRaceCar()
-       Purpose: removes a race car entry from the database
-       Return Type: boolean
-       Argument: int vehicleID
+    /**
+     * This method removes a race car from the database.
+     * @param vehicleID the id of the car the user wants to remove
+     * @return boolean true or false value to determine if the operation was successful
      */
     public boolean removeRaceCar(int vehicleID) {
         System.out.println("Removing Race Car...");
@@ -103,6 +123,16 @@ public class RaceCarDBManager extends RaceCarManager{
         return true;
     }
 
+    /**
+     * This method performs updates to the database, modifying existing information.
+     * @param id the id of the car
+     * @param make the make of the car
+     * @param model the model of the car
+     * @param year the year that the car was made
+     * @param topSpeed the top speed that the car can perform
+     * @param hasLaunched the boolean value that represents the launch status of the car
+     * @return boolean true or false value that determines if the operation was successful
+     */
     public boolean updateRaceCar(int id, String make, String model, int year, double topSpeed, boolean hasLaunched) {
         System.out.println("Updating Race Car...");
 
@@ -136,10 +166,9 @@ public class RaceCarDBManager extends RaceCarManager{
         return true;
     }
 
-    /* Method: getRaceCar()
-       Purpose: converts sql select all query into an array list
-       Return Type: ArrayList
-       Argument: RaceCar raceCar
+    /**
+     * This method converts a SQL select query into an array list.
+     * @return the result of the query as an array list
      */
     @Override
     public ArrayList<RaceCar> getRaceCars() {
@@ -169,12 +198,12 @@ public class RaceCarDBManager extends RaceCarManager{
         return raceCars;
     }
 
-    /* Method: launchRaceCars()
-       Purpose: simulates a race between two race cars by probability
-       Return Type: RaceCar
-       Arguments: RaceCar raceCar1, RaceCar raceCar2
+    /**
+     * This method simulates a race between two cars.
+     * @param raceCar1 the first car to be raced
+     * @param raceCar2 the second car to be raced against
+     * @return RaceCar the winner of the race
      */
-
     public RaceCar launchRaceCars(RaceCar raceCar1, RaceCar raceCar2) {
         System.out.println("Launching Race Car...");
 
@@ -202,10 +231,12 @@ public class RaceCarDBManager extends RaceCarManager{
     }
 
     // Validation Methods
-    /* Method: idValid()
-       Purpose: validates id for adding usage: checks if id is unique and non-negative
-       Return Type: boolean
-       Arguments: int
+
+    /**
+     * This method performs validation of the vehicle id. This checks if there is an existing id or if there
+     * is a non-negative number.
+     * @param id the id to be checked
+     * @return boolean true or false value to determine if the id was valid
      */
     public boolean idValid(int id) {
         for (RaceCar raceCar : getRaceCars()) {
@@ -222,6 +253,10 @@ public class RaceCarDBManager extends RaceCarManager{
         return true;
     }
 
+    /**
+     * This method checks if the class has a connection.
+     * @return boolean true or false value to determine if there is a connection
+     */
     public boolean hasConnection() {
         if (getConnection() == null) {
             return false;

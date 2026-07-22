4e61584537e30e8e2553d6ea1fd64e5663251ef0
Ashbel Legros <ashbel.legros@gmail.com>
2026-07-21 22:10:37 -0400

    Update RaceCarManager.java with documentation

--- src/RaceCarManager.java
+++ src/RaceCarManager.java
@@ -1,37 +1,44 @@
-/*
- * Name : Ashbel Legros
- * Course : Software Development I
- * Date : 07/03/26
- * Class : RaceCarManager
- * Purpose : Contains validation for business logic.
- * */
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
+/**
+ * This parent class provides validation logic for RaceCarDBManager class. Its primary role is to ensure proper
+ * creation of data as well as modification to data.
+ *
+ * @author Ashbel Legros
+ **/
 public class RaceCarManager {
     private ArrayList<RaceCar> raceCars;
 
+    /**
+     * Initializes a new empty array list which holds race car objects.
+     */
     public RaceCarManager() {
         raceCars = new ArrayList<>();
     }
 
+    /**
+     * This method gets the array list of cars.
+     * @return the array list of car objects
+     */
     public ArrayList<RaceCar> getRaceCars() {
         return raceCars;
     }
 
+    /**
+     * This method sets the value of raceCars to the parameter.
+     * @param raceCars an array of race car objects
+     */
     public void setRaceCars(ArrayList<RaceCar> raceCars) {
         this.raceCars = raceCars;
     }
 
     //Validation Methods
 
-
-
-    /* Method: idFound()
-       Purpose: finds existing Car by id
-       Return Type: RaceCar
-       Arguments: int
+    /**
+     * This method was used to check if an id was found in an array list of race car objects.
+     * @param id the id to be found
+     * @return RaceCar if the id is found, the raceCar object is returned
      */
     public RaceCar idFound(int id) {
         for (int i = 0; i<raceCars.size(); i++) {
@@ -50,11 +57,10 @@ public class RaceCarManager {
         return null;
     }
 
-
-    /* Method: stringValid()
-       Purpose: validates string input: checks if user left input empty or with spaces
-       Return Type: boolean
-       Arguments: String
+    /**
+     * This method checks if string input is valid. It checks if there is empty input or full of spaces.
+     * @param inputString the string to be checked
+     * @return boolean true or false value to determine if the string input is valid
      */
     public boolean stringValid(String inputString){
 
@@ -71,10 +77,10 @@ public class RaceCarManager {
         return true;
     }
 
-    /* Method: yearValid()
-       Purpose: validates year input: checks if the year is in certain range
-       Return Type: boolean
-       Arguments: int
+    /**
+     * This method checks if year input is valid. It checks if the year is in the range of 2000-2026
+     * @param year the year to be checked
+     * @return boolean true or false value to determine if the year input is valid
      */
     public boolean yearValid(int year) {
 
@@ -86,10 +92,11 @@ public class RaceCarManager {
         return true;
     }
 
-    /* Method: topSpeedValid()
-       Purpose: validates topSpeedinput: checks if input uses one decimal place and checks if input is non-negative
-       Return Type: boolean
-       Arguments: double
+    /**
+     * This method checks if the top speed input is valid. It checks if the user is using one decimal place or
+     * if the speed is positive.
+     * @param topSpeed the top speed input to be checked
+     * @return boolean true or false value to determine if the top speed input is valid
      */
     public boolean topSpeedValid(double topSpeed) {
         if (BigDecimal.valueOf(topSpeed).scale() > 1 || BigDecimal.valueOf(topSpeed).scale() < 1) {
@@ -105,10 +112,10 @@ public class RaceCarManager {
         return true;
     }
 
-    /* Method: launchValid()
-       Purpose: validates launchStatus: check if user is entering either 0 or 1
-       Return Type: boolean
-       Arguments: int
+    /**
+     * This method checks if the launch status is valid. It checks if it has a value of 0 or 1.
+     * @param launchStatus the launch status input to be checked
+     * @return boolean true or false value to determine if the launch status input is valid
      */
     public boolean launchValid(int launchStatus) {
 
@@ -120,10 +127,12 @@ public class RaceCarManager {
         return true;
     }
 
-    /* Method: raceValid()
-       Purpose: validates race conditions: checks if either car has launched already, if vehicle id is the same, and if car exists
-       Return Type: boolean
-       Arguments: RaceCar, RaceCar
+    /**
+     * This method checks if the conditions of the race are valid. It checks if the car objects are not null,
+     * if the cars racing are two separate cars, and if either car has been launched already.
+     * @param raceCar1 the first race car to be checked
+     * @param raceCar2 the second race car to be checked
+     * @return boolean true or false to determine if the race conditions have been met
      */
     public boolean raceValid(RaceCar raceCar1,  RaceCar raceCar2) {
         if (raceCar1 == null || raceCar2 == null) {
